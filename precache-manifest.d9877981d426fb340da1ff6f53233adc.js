7ae8b3deb85a0ac21b18dec55622930d8b6ccb78
Andrecio Bezerra <andreciobezerra@gmail.com>
2020-03-21 15:44:19 -0300

    Updates

---
+++ precache-manifest.d9877981d426fb340da1ff6f53233adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7402dde803059e462c2ad252f692e8ea",
+    "url": "/weather-report/index.html"
+  },
+  {
+    "revision": "454c9e46055750df181b",
+    "url": "/weather-report/static/js/2.3388ebab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-report/static/js/2.3388ebab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a486607f370b248127e",
+    "url": "/weather-report/static/js/main.fab8c250.chunk.js"
+  },
+  {
+    "revision": "f98647b4b903d3fa2876",
+    "url": "/weather-report/static/js/runtime-main.d59a9cbc.js"
+  }
+]);
